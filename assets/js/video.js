af76c9b07f3d2dfee4adf8925fb760da315ec6a4
devarify <arify@outlook.co.id>
2020-09-01 15:46:52 +0700

    01092020

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,23 +1,2 @@
-	var vid = document.getElementById("bgvid");
-	var pauseButton = document.querySelector("#polina button");
-
-	function vidFade() {
-	  vid.classList.add("stopfade");
-	}
-
-	vid.addEventListener('ended', function() {
-	// only functional if "loop" is removed 
-	vid.pause();
-	// to capture IE10
-	vidFade();
-	}); 
-	pauseButton.addEventListener("click", function() {
-	  vid.classList.toggle("stopfade");
-	  if (vid.paused) {
-	    vid.play();
-	    pauseButton.innerHTML = "<i class='lni-pause'></i>";
-	  } else {
-	    vid.pause();
-	    pauseButton.innerHTML = "<i class='lni-play'></i>";
-	  }
-	})
+var vid=document.getElementById("bgvid");var pauseButton=document.querySelector("#polina button");function vidFade(){vid.classList.add("stopfade");}
+vid.addEventListener('ended',function(){vid.pause();vidFade();});pauseButton.addEventListener("click",function(){vid.classList.toggle("stopfade");if(vid.paused){vid.play();pauseButton.innerHTML="<i class='lni-pause'></i>";}else{vid.pause();pauseButton.innerHTML="<i class='lni-play'></i>";}})
